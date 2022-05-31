e66424ae0e6185fdd8af0b8c01779427afa63e7a
Brayan Hernández López <98982873+brayanhl@users.noreply.github.com>
2022-05-30 19:23:43 -0500

    Js de administrador

---
+++ assets/js/admin-pal.js
@@ -0,0 +1,72 @@
+var Id = document.getElementById('id');
+var Palabra = document.getElementById('palabra');
+var Traduccion = document.getElementById('traduccion');
+var Idioma = document.getElementById('idioma');
+var Tema = document.getElementById('tema');
+var Rareza = document.getElementById('rareza');
+
+function insertar(){
+    $.ajax({
+        type: 'GET',
+        url: `http://kaanbal.azurewebsites.net/Conexion.asmx/insPalabra?palabra=${Palabra.value}&traduccion=${Traduccion.value}&rareza=${Rareza.value}&idioma=${Idioma.value}&tema=${Tema.value}`,
+        dataType: 'xml'
+    }).done((data) => {
+        switch($(data).find('Bandera').text()) {
+            case '1':
+                alert("¡Registro exitoso!");
+            break;
+            case '-1':
+                alert("El tema no existe");
+            break;
+            case '0':
+                alert("La palabra ya existe");
+            break;
+            default: 
+            alert("Inserción incorrecta, verifique los datos ingresados");
+        }
+        console.log($(data).find('Bandera').text());
+    }) 
+}
+
+function modificar(){
+    $.ajax({
+        type: 'GET',
+        url: `http://kaanbal.azurewebsites.net/Conexion.asmx/updPalabra?id=${Id.value}&palabra=${Palabra.value}&traduccion=${Traduccion.value}&rareza=${Rareza.value}&idioma=${Idioma.value}&tema=${Tema.value}`,
+        dataType: 'xml'
+    }).done((data) => {
+        switch($(data).find('Bandera').text()) {
+            case '1':
+                alert("¡Modificación exitosa!");
+            break;
+            case '-1':
+                alert("El tema no existe");
+            break;
+            case '0':
+                alert("La palabra ya existe");
+            break;
+            default: 
+            alert("Modificación incorrecta, verifique los datos ingresados");
+        }
+        console.log($(data).find('Bandera').text());
+    }) 
+}
+
+function eliminar(){
+    $.ajax({
+        type: 'GET',
+        url: `http://kaanbal.azurewebsites.net/Conexion.asmx/DelPalabra?id=${Id}`,
+        dataType: 'xml'
+    }).done((data) => {
+        switch($(data).find('Bandera').text()) {
+            case '1':
+                alert("¡Eliminación exitosa!");
+            break;
+            case '-1':
+                alert("La clave no existe");
+            break;
+            default: 
+            alert("Eliminación incorrecta, verifique los datos ingresados");
+        }
+        console.log($(data).find('Bandera').text());
+    }) 
+}
---
+++ assets/js/admin-usu.js
@@ -0,0 +1,66 @@
+var Id = document.getElementById('id');
+var Nombre = document.getElementById('nombre');
+var Correo = document.getElementById('correo');
+var Paterno = document.getElementById('paterno');
+var Materno = document.getElementById('materno');
+var Contrasena = document.getElementById('contrasena');
+
+function insertar(){
+    $.ajax({
+        type: 'GET',
+        url: `http://kaanbal.azurewebsites.net/Conexion.asmx/insUsuario?nombre=${Nombre.value}&contrasena=${Contrasena.value}&correo=${Correo.value}&ap_pat=${Paterno.value}&ap_mat=${Materno.value}&tip_id=${Tipo}`,
+        dataType: 'xml'
+    }).done((data) => {
+        switch($(data).find('Bandera').text()) {
+            case '1':
+                alert("¡Registro exitoso!");
+            break;
+            case '-1':
+                alert("El correo ya existe");
+            break;
+            default: 
+            alert("Inserción incorrecta, verifique los datos ingresados");
+        }
+        console.log($(data).find('Bandera').text());
+    }) 
+}
+
+function modificar(){
+    $.ajax({
+        type: 'GET',
+        url: `http://kaanbal.azurewebsites.net/Conexion.asmx/updUsuario?id=${Id.value}&nombre=${Nombre.value}&contrasena=${Contrasena.value}&correo=${Correo.value}&ap_pat=${Paterno.value}&ap_mat=${Materno.value}&tip_id=${Tipo}`,
+        dataType: 'xml'
+    }).done((data) => {
+        switch($(data).find('Bandera').text()) {
+            case '1':
+                alert("¡Modificación exitosa!");
+            break;
+            case '-1':
+                alert("El correo ya existe");
+            break;
+            default: 
+            alert("Modificación incorrecta, verifique los datos ingresados");
+        }
+        console.log($(data).find('Bandera').text());
+    }) 
+}
+
+function eliminar(){
+    $.ajax({
+        type: 'GET',
+        url: `http://kaanbal.azurewebsites.net/Conexion.asmx/DelUsuario?id=${Id.value}`,
+        dataType: 'xml'
+    }).done((data) => {
+        switch($(data).find('Bandera').text()) {
+            case '1':
+                alert("¡Eliminación exitosa!");
+            break;
+            case '-1':
+                alert("La clave no existe");
+            break;
+            default: 
+            alert("Eliminación incorrecta, verifique los datos ingresados");
+        }
+        console.log($(data).find('Bandera').text());
+    }) 
+}
